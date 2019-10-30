90c56b54eac65c45bcaa3f38e7bd3dc2b1384883
Robert Bauer <robert.scott.bauer@gmail.com>
2019-10-30 11:00:46 -0400

    Added in function for writing to MongoDB database 10/30/2019

--- indexparse.py
+++ indexparse.py
@@ -2,8 +2,28 @@
 """
 Script for taking SEC indexes and downloading the associated forms then placing the meta data for those forms in a MongoDB database
 """
-
+import pymongo as py
+from py import MongoClient
 import pandas as pd
 
-df = pd.read_csv("E:/SEC/Indexes/Testing/1999QTR3.txt",'|',header = None, names = [1,2,3,4,5], skiprows = 11)
+client = MongoClient('localhost',27017)
+db = client.testbase
 
+
+df = pd.read_csv("C:\\Users\\Robert Bauer\\Documents\\2015QTR1.txt",'|', header = None, names = [1,2,3,4,5], skiprows = 11)
+
+print(df.shape[0])
+print(type(df.iloc[0,1]))
+
+def indexparser(indexframe):
+    for row in range(indexframe.shape[0]):
+        entry = {"CIK": indexframe.iloc[row,1],
+                 "Company Name": indexframe.iloc[row,2],
+                 "Form Type": indexframe.iloc[row,3],
+                 "Date Filed": indexframe.iloc[row,4],
+                 "Filename": indexframe.iloc[row,5]
+             }
+        record = db.test.insert_one(entry)
+    return record;
+
+print("Done")
